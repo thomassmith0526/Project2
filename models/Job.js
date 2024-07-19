b5691fedc481f30225a38aa1db68ab93e2dc805d
Kalink52 <micah.cox52@gmail.com>
2024-07-19 19:55:22 -0400

    added references to job table

--- models/Job.js
+++ models/Job.js
@@ -5,15 +5,30 @@ const sequelize = require('../config/connection.js');
 class Job extends Model {}
 
 Job.init({
-    job_id: {
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true 
     },
     client_id: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'client',
+            keys: 'id'
+          }
     },
     employee_id: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'employee',
+            keys: 'id'
+          }
+    },
+    task_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'task',
+            keys: 'id'
+          }
     },
     job_date: {
         type: DataTypes.STRING(40)
