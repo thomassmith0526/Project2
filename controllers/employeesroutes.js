57e23441628eaafca33472b99a3e2b78cfc64a83
thomassmith0526 <thomassmith0526@gmail.com>
2024-07-19 21:34:30 -0500

    comment out render till handlebars are ready

--- controllers/employeesroutes.js
+++ controllers/employeesroutes.js
@@ -5,8 +5,8 @@ router.get('/', async (req, res) => {
     const employeeData = await Employee.findAll().catch((err) => {
         res.json(err);
     })
-    const employees = employeeData.map((employee) => employee.get({ plain: true }));
-    res.render('', {employees})
+//     const employees = employeeData.map((employee) => employee.get({ plain: true }));
+//     res.render('', {employees})
 })
 
 router.get('/employees/:id', async (req, res) => {
@@ -16,8 +16,8 @@ router.get('/employees/:id', async (req, res) => {
             res.status(404).json({ message: 'There is no employee with that id'});
             return;
         }
-        const employee = employeeData.get({ plain: true});
-        res.render('',employee);
+    //     const employee = employeeData.get({ plain: true});
+    //     res.render('',employee);
     } catch (err) {
         res.status(500).json(err);
     }
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
+const routes = require('./controllers')
 const app = express();
 const PORT = process.env.PORT || 3001;
+// const sequelize = require('./config/connection')
+
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log('microphone check');
