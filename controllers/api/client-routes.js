e38a3d51d91fee1268e2cfa0b9b7d7340d62799c
dstrzykalski <dstrzykalski@yahoo.com>
2024-07-22 14:12:55 -0400

    Added Client Routes

---
+++ controllers/api/client-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const Client = require('../../models/Client')
+
+router.get('/', async (req, res) => {
+    const clientData = await Client.findAll().catch((err) => {
+        res.json(err);
+    })
+    res.json(clientData)
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const clientData = await Client.findByPk(req.params.id);
+        if(!clientData) {
+            res.status(404).json({ message: 'There is no client with that id'});
+            return;
+        }
+        res.json(clientData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const clientData = await Client.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+        });
+        res.status(200).json(clientData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const employeeRoutes = require('./employees-routes.js')
+const clientRoutes = require('./client-routes.js')
 
 router.use('/employees', employeeRoutes)
+router.use('/clients', clientRoutes)
 
 module.exports = router;
