f32479802824168aac3adbc5040fe1ed2db9a193
TheR16H <rashawn5hall@gmail.com>
2024-07-22 19:24:11 -0400

    tasks

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const employeeRoutes = require('./employees-routes.js')
 const clientRoutes = require('./client-routes.js')
+const taskRoutes = require('./tasks-routes.js');
 
 router.use('/employees', employeeRoutes)
 router.use('/clients', clientRoutes)
+router.use('/tasks', taskRoutes);
 
 module.exports = router;
