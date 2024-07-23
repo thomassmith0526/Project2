1a3721e8fe7fd847d4e414ad2d0e760b036b54bb
bl00dy hazak <kennethhass1014@gmail.com>
2024-07-23 17:07:53 -0400

    job route test

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 const employeeRoutes = require('./employees-routes.js')
 const clientRoutes = require('./client-routes.js')
 
+const jobRoutes = require('./job-routes.js')
+
 router.use('/employees', employeeRoutes)
 router.use('/clients', clientRoutes)
+
+router.use('/jobs', jobRoutes)
 
 module.exports = router;
