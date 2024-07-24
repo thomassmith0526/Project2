74228385dd29b7a3bbcc0f830fb286daeca3b7b2
Kalink52 <micah.cox52@gmail.com>
2024-07-24 18:14:49 -0400

    inital commit

--- seeds/job-seeds.js
+++ seeds/job-seeds.js
@@ -11,30 +11,30 @@ const jobData =[
         comments: 'Gate code is 7373',
         isDone: false
     },
-    // {
-    //     client_id: '3',
-    //     employee_id: '2', 
-    //     task_id: '1',
-    //     date:'9/26/23',
-    //     comments:'They like strips cut in there grass',
-    //     isDone: false
-    // },
-    // {
-    //     client_id: '2',
-    //     employee_id: '1',
-    //     task_id: '3',
-    //     date:'5/26/24',
-    //     comments:'',
-    //     isDone: true
-    // },
-    // {
-    //     client_id: '2',
-    //     employee_id: '1',
-    //     task_id: '2',
-    //     date:'6/23/24',
-    //     comments:'',
-    //     isDone: false
-    // }
+    {
+        client_id: '3',
+        employee_id: '2', 
+        task_id: '1',
+        date:'9/26/23',
+        comments:'They like strips cut in there grass',
+        isDone: false
+    },
+    {
+        client_id: '2',
+        employee_id: '1',
+        task_id: '3',
+        date:'5/26/24',
+        comments:'',
+        isDone: true
+    },
+    {
+        client_id: '2',
+        employee_id: '1',
+        task_id: '2',
+        date:'6/23/24',
+        comments:'',
+        isDone: false
+    }
 ];
 
 const jobsDone = () => Job.bulkCreate(jobData);
