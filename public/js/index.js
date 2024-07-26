83d6943da9b66b1685c325b297094e5e5f7989c0
thomassmith0526 <thomassmith0526@gmail.com>
2024-07-25 19:41:33 -0500

    help

---
+++ public/js/index.js
@@ -0,0 +1,49 @@
+async function newFormHandler(event) {
+    event.preventDefault()
+    console.log('shhh micah')
+    
+    const first_name = document.getElementById('first_name').value;
+    console.log()
+    const last_name = document.getElementById('last_name').value;
+    const location = document.getElementById('location').value;
+    const task = document.getElementById('task').value
+
+    const response = await fetch(`api/client`, {
+        method: 'POST',
+        body: JSON.stringify({
+            first_name,
+            last_name,
+            location,
+        }),
+      
+    });
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to add Client')
+    }
+    const responseTask = await fetch(`api/jobs`, {
+        method: 'POST',
+        body: JSON.stringify({
+            task
+        }),
+    });
+    if(responseTask.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to add Task')
+    }
+}
+addEventListener('submit', newFormHandler)
+document.querySelector('.form-group')
+    
+    
+    console.log('ahahahahaahah')
+
+// const checkbox = document.getElementById('done');
+// checkbox.addEventListener('change', function(){
+//     const isChecked = checkbox.checked;
+//     const template = Handlebars.compile(document.getElementById('template').innerHTML);
+//     const context = {checkbox: isChecked};
+//     document.getElementById('output').innerHTML = html;
+// });
