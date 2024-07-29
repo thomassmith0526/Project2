c126ae7706b4239421bad72bb448d98f13019003
Ernesto Gonzalez <ernestoale37@gmail.com>
2024-07-28 23:40:35 -0400

    Style is mostly improved

--- seeds/job-seeds.js
+++ seeds/job-seeds.js
@@ -6,7 +6,7 @@ const jobData =[
         client_id: '1',
         employee_id: 2,
         task_id: '3',
-        date: '2024-06-02',
+        date: '02-06-2024',
         comments: 'Gate code is 7373',
         isDone: true
     },   
@@ -14,7 +14,7 @@ const jobData =[
         client_id: '3',
         employee_id: '2', 
         task_id: '1',
-        date:'2024-09-26',
+        date:'26-09-2024',
         comments:'They like strips cut in there grass',
         isDone: false
     },
@@ -22,7 +22,7 @@ const jobData =[
         client_id: '2',
         employee_id: '1',
         task_id: '3',
-        date:'2024-05-26',
+        date:'26-05-2024',
         comments:'',
         isDone: true
     },
@@ -30,7 +30,7 @@ const jobData =[
         client_id: '2',
         employee_id: '1',
         task_id: '2',
-        date:'2024-06-23',
+        date:'23-06-2024',
         comments:'',
         isDone: true
     }
