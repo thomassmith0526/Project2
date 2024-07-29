54a0dcadd17643cb8310a24245bb07ddb6a77c12
TheR16H <rashawn5hall@gmail.com>
2024-07-29 19:53:55 -0400

    removed unused code/updated readme/ added visuals

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,16 +48,4 @@ router.get('/tasks', async (req, res) => {
     }
 });
 
-// router.get('/', async (req,res) =>{
-//     const jobData = await Job.findAll()
-//     const taskData = await Task.findAll()
-//     const employeeData = await Employee.findAll()
-//     const clientData = await Client.findAll()
-//     const jobs = jobData.map((job) => job.get({ plain: true}))
-//     const tasks = taskData.map((task) => task.get({ plain: true}))
-//     const employees = employeeData.map((employee) => employee.get({ plain: true}))
-//     const clients = clientData.map((client) => client.get({plain:true}))
-//     res.render('all',{jobs,tasks, employees, clients})
-// })
- 
 module.exports = router;
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,11 @@
 async function clientHandler(event) {
     event.preventDefault()
-    // console.log('shhh micah')
     
     const first_name = document.getElementById('first_name').value;
 
     const last_name = document.getElementById('last_name').value;
     const location = document.getElementById('location').value;
-    // const task = document.getElementById('task').value
-    //Added this to handle empty fields
+    
     if (first_name == "" || last_name == "") {
         window.alert("Please, input required fields before adding client");
         return;
